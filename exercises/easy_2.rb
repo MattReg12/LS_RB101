7a1b77802d141411b56afe919ec9725aae7c9760
Matt Register <rmregister0510@gmail.com>
2022-02-10 06:43:37 -0500

    Completed easy_2 set of questions. 1 hour spent. 2_10_2022

--- exercises/easy_2.rb
+++ exercises/easy_2.rb
@@ -4,7 +4,7 @@ def display_teddys_age_randomly
   puts 'Teddy is %<random_age>s years old!' % { random_age: rand(20..200) }
 end
 
-display_teddys_age_randomly
+# display_teddys_age_randomly
 
 # Modify this program to ask for a name, and then print the age for that person. For an extra challenge, use "Teddy" as the name if no name is entered.
 
@@ -65,4 +65,115 @@ def calculate_tip
   puts 'The total is $%<total>s' % { total: total }
 end
 
-calculate_tip
+# calculate_tip
+
+# 4) Build a program that displays when the user will retire and how many years
+# she has to work till retirement.
+
+=begin
+What is your age? 30
+At what age would you like to retire? 70
+
+It's 2016. You will retire in 2056.
+You have only 40 years of work to go!
+=end
+
+def display_retirement_age
+  puts 'What is your age? '
+  current_age = gets.chomp.to_i
+  puts 'At what age would you like to retire? '
+  retirement_age = gets.chomp.to_i
+  current_year = Time.now.year
+  years_remaining = retirement_age - current_age
+  output = <<~OUTPUT
+    Its %<current_year>s. You will retire in %<retirement_year>s.
+    You have only %<years_remaining>s years of work to go!
+  OUTPUT
+ puts output % { current_year: current_year, retirement_year: current_year + years_remaining, years_remaining: years_remaining }
+end
+
+# display_retirement_age
+
+# 5) Write a program that will ask for user's name. The program will then greet the user.
+# If the user writes "name!" then the computer yells back to the user.
+
+def display_greeting
+  puts 'What is your name? '
+  name = gets.chomp
+  puts name.end_with?('!') ? "HELLO #{name.upcase} WHY ARE WE SCREAMING?" : "Hello #{name}."
+end
+
+# display_greeting
+
+# 6) Print all odd numbers from 1 to 99, inclusive, to the console, with each number on a separate line.
+
+def display_odd_numbers
+  (1..99).step(2) { |odd| puts odd }
+end
+
+# display_odd_numbers
+
+# 7) Print all even numbers from 1 to 99, inclusive, to the console, with each number on a separate line.
+
+def display_even_numbers
+  (1..99).each { |number| puts number if number.even? }
+end
+
+# display_even_numbers
+
+# 8) Write a program that asks the user to enter an integer greater than 0, then asks if the
+# user wants to determine the sum or product of all numbers between 1 and the entered integer.
+
+=begin
+>> Please enter an integer greater than 0:
+5
+>> Enter 's' to compute the sum, 'p' to compute the product.
+s
+The sum of the integers between 1 and 5 is 15.
+>> Please enter an integer greater than 0:
+6
+>> Enter 's' to compute the sum, 'p' to compute the product.
+p
+The product of the integers between 1 and 6 is 720.
+=end
+
+def display_consecutive_int_operation
+  puts 'Please enter an integer greater than 0: '
+  numbers = (1..gets.chomp.to_i)
+  puts 'Enter \'s\' to compute the same, \'p\' to compute the product.'
+  operator = gets.chomp
+  if operator == 'p'
+    puts 'The product of integers between 1 and %<max>s is %<answer>s. ' % { max: numbers.max, answer: numbers.reduce(&:*) }
+  elsif operator == 's'
+    puts 'The sum of integers between 1 and %<max>s is %<answer>s. ' % { max: numbers.max, answer: numbers.reduce(&:+) }
+  else puts 'Invalid operation! Thanks.'
+  end
+end
+
+display_consecutive_int_operation
+
+# 9) Take a look at the following code: What does this code print out? Think about it for a moment before continuing.
+
+name = 'Bob'
+save_name = name
+name = 'Alice'
+puts name, save_name
+
+name = 'Bob'
+save_name = name
+name.upcase!
+puts name, save_name
+
+# What does this print out? Can you explain these results?
+
+# BOB, BOB because both variables point to the same space in memory and upcase! is a destructive method.
+
+# 10)  What will the following code print, and why? Don't run the code until you have tried to answer.
+
+array1 = %w(Moe Larry Curly Shemp Harpo Chico Groucho Zeppo)
+array2 = []
+array1.each { |value| array2 << value }
+array1.each { |value| value.upcase! if value.start_with?('C', 'S') }
+puts array2
+
+# The strings in array2 are the same objects as the ones in array 1. So the upcase! mutates them as well.
