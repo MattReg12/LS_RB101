5007333cebb62e2766bb997777aef75e2a9c429a
Matt Register <rmregister0510@gmail.com>
2022-02-17 14:28:55 -0500

    Completed easy_5 set of questions. Time spent: 40 minutes. 2_17_2022

--- exercises/easy_5.rb
+++ exercises/easy_5.rb
@@ -105,9 +105,115 @@ def word_sizes(string)
 end
 
 
-p word_sizes('Four score and seven.') == { 3 => 1, 4 => 1, 5 => 1, 6 => 1 }
-p word_sizes('Hey diddle diddle, the cat and the fiddle!') == { 3 => 5, 6 => 1, 7 => 2 }
-p word_sizes("What's up doc?") == { 6 => 1, 2 => 1, 4 => 1 }
-p word_sizes('') == {}
+# p word_sizes('Four score and seven.') == { 3 => 1, 4 => 1, 5 => 1, 6 => 1 }
+# p word_sizes('Hey diddle diddle, the cat and the fiddle!') == { 3 => 5, 6 => 1, 7 => 2 }
+# p word_sizes("What's up doc?") == { 6 => 1, 2 => 1, 4 => 1 }
+# p word_sizes('') == {}
 
-# 7)
+# 7) Modify the word_sizes method from the previous exercise to exclude non-letters when
+# determining word size. For instance, the length of "it's" is 3, not 4.
+
+def second_word_sizes(string)
+  string = string.gsub(/[^a-zA-Z ]/, '')
+  word_sizes(string)
+end
+
+# p second_word_sizes('Four score and seven.') == { 3 => 1, 4 => 1, 5 => 2 }
+# p second_word_sizes('Hey diddle diddle, the cat and the fiddle!') == { 3 => 5, 6 => 3 }
+# p second_word_sizes("What's up doc?") == { 5 => 1, 2 => 1, 3 => 1 }
+# p second_word_sizes('') == {}
+
+# 8) Write a method that takes an Array of Integers between 0 and 19, and returns an Array of those
+# Integers sorted based on the English words for each number:
+
+# zero, one, two, three, four, five, six, seven, eight, nine, ten, eleven, twelve, thirteen,
+# fourteen, fifteen, sixteen, seventeen, eighteen, nineteen
+
+NUM_WORDS = { 1 => 'one', 2 => 'two', 3 => 'three', 4 => 'four', 5 => 'five', 6 => 'six', 7 => 'seven',
+              8 => 'eight', 9 => 'nine', 10 => 'ten', 11 => 'eleven', 12 => 'tweleve', 13 => 'thirteen',
+              14 => 'fourteen', 15 => 'fifteen', 16 => 'sixteen', 17 => 'seventeen', 18 => 'eighteen',
+              19 => 'nineteen', 0 => 'zero' }
+
+def alphabetic_number_sort(array)
+  array.sort_by { |number| NUM_WORDS[number] }
+end
+
+=begin
+p alphabetic_number_sort((0..19).to_a) == [
+  8, 18, 11, 15, 5, 4, 14, 9, 19, 1, 7, 17,
+  6, 16, 10, 13, 3, 12, 2, 0
+]
+=end
+
+# 9) Write a method that takes a string argument and returns a new string that contains the
+# value of the original string with all consecutive duplicate characters collapsed into a
+# single character. You may not use String#squeeze or String#squeeze!.
+
+def crunch(string)
+  arr = string.chars
+  arr.each_with_object('').with_index do |(letter, sum), index|
+    sum << letter if letter != arr[index + 1]
+  end
+end
+
+# p crunch('ddaaiillyy ddoouubbllee') == 'daily double'
+# p crunch('4444abcabccba') == '4abcabcba'
+# p crunch('ggggggggggggggg') == 'g'
+# p crunch('a') == 'a'
+# p crunch('') == ''
+
+# or with regex...
+
+def crunch(string)
+  string.gsub(/.{2}/) { |hit| hit[0] }
+end
+
+p crunch('ddaaiillyy ddoouubbllee') #== 'daily double'
+p crunch('4444abcabccba')# == '4abcabcba'
+p crunch('ggggggggggggggg') #== 'g'
+p crunch('a') == 'a'
+p crunch('') == ''
+
+# 10) Write a method that will take a short line of text, and print it within a box.
+
+def print_in_box(string)
+  size = string.length
+  puts "+-#{'-' * size}-+"
+  puts "| #{' ' * size} |"
+  puts "| #{string.center(size)} |"
+  puts "| #{' ' * size} |"
+  puts "+-#{'-' * size}-+"
+end
+
+print_in_box('To boldly go where no one has gone before.')
+print_in_box('')
+
+=begin
++--------------------------------------------+
+|                                            |
+| To boldly go where no one has gone before. |
+|                                            |
++--------------------------------------------+
+
+print_in_box('')
++--+
+|  |
+|  |
+|  |
++--+
+=end
+
+# 11) You are given a method named spin_me that takes a string as an argument and returns
+# a string that contains the same words, but with each word's characters reversed. Given
+# the method's implementation, will the returned string be the same object as the one
+# passed in as an argument or a different object?
+
+def spin_me(str)
+  str.split.each do |word|
+    word.reverse!
+  end.join(" ")
+end
+
+spin_me("hello world") # "olleh dlrow"
+
+# different. We are creating all diff objects here.
