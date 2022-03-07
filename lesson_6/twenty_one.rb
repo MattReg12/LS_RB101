4d1054df5393d55be4ba19d4e91bccf4b02ac6bd
Matt Register <rmregister0510@gmail.com>
2022-03-07 14:36:31 -0500

    Added more messages. Added ace_handler, busted?, player_turn, player_input, and decision_valid? methods. Started main loop structure.

--- lesson_6/twenty_one.rb
+++ lesson_6/twenty_one.rb
@@ -8,7 +8,7 @@ DECK_VALUES = {'2' => 2, '3' => 3, '4' => 4, '5' => 5, '6' => 6, '7' => 7,
 MESSAGES = YAML.load_file('msgs.yml')
 
 def prompt(message)
-  puts '=> %s' % message
+  puts "=> %s\n\n" % message
 end
 
 def deck
@@ -16,14 +16,71 @@ def deck
   deck.map(&:join)
 end
 
-def deal_card(deck, hand)
+def deal_card(hand, deck)
   hand << deck.pop
 end
 
-def display_hand(player, hand)
-  prompt(MESSAGES[display_hand] % [player, hand.join(' and '), value(hand)}
+def deal_starting_hands(player_hand, dealer_hand, deck)
+  2.times { deal_card(dealer_hand, deck) }
+  2.times { deal_card(player_hand, deck) }
+end
+
+def display_hands(participant, hand)
+  prompt(MESSAGES[participant] % [hand.join(' and '), value(hand)])
 end
 
 def value(hand)
-  hand.reduce { |card| DECK_VALUES[card[0]] }
+  total = hand.reduce(0) { |sum, card| sum + DECK_VALUES[card[0]] }
+  ace_handler(hand, total)
+end
+
+def ace_handler(hand, total)
+  return total if hand.none? { |card| card[0] == 'A' }
+
+  total <= 21 ? total : (total - 10)
+end
+
+def busted?(hand)
+  value(hand) > 21
+end
+
+def player_turn(hand, deck)
+  loop do
+    decision = player_input
+    break if decision == 'stay'
+    deal_card(player_hand, deck)
+    break if busted?(hand)
+  end
+end
+
+def player_input
+  loop do
+    prompt(MESSAGES['player_decision'])
+    decision = gets.chomp
+    break decision if decision_valid?
+    prompt(MESSAGES['invalid_decision'])
+  end
+end
+
+def decision_valid?(decision)
+  ['hit', 'stay', 'h', 's'].include?(decision.downcase)
+end
+
+
+loop do
+  prompt(MESSAGES['welcome'])
+  loop do
+    current_deck = deck.shuffle
+    player_hand = []
+    dealer_hand = []
+
+    prompt(MESSAGES['dealing'])
+    deal_starting_hands(player_hand, dealer_hand, current_deck)
+    sleep(3)
+
+    display_hand('dealer_before', [dealer_hand.first])
+    display_hand('player', player_hand)
+    break
+  end
+  break
 end
