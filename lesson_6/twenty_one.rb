dbad1136bccf553c50e22256036645ad520ea682
Matt Register <rmregister0510@gmail.com>
2022-03-07 07:04:17 -0500

    Added prompt, deck, deal_card, display_hand, and value methods. Added display_hand message to yml file.

--- lesson_6/twenty_one.rb
+++ lesson_6/twenty_one.rb
@@ -6,3 +6,24 @@ DECK_VALUES = {'2' => 2, '3' => 3, '4' => 4, '5' => 5, '6' => 6, '7' => 7,
                '8' => 8, '9' => 9, '1' => 10, 'J' => 10, 'Q' => 10,
                'K' => 10, 'A' => 11 }
 MESSAGES = YAML.load_file('msgs.yml')
+
+def prompt(message)
+  puts '=> %s' % message
+end
+
+def deck
+  deck = CARD_NAMES.product(SUITS)
+  deck.map(&:join)
+end
+
+def deal_card(deck, hand)
+  hand << deck.pop
+end
+
+def display_hand(player, hand)
+  prompt(MESSAGES[display_hand] % [player, hand.join(' and '), value(hand)}
+end
+
+def value(hand)
+  hand.reduce { |card| DECK_VALUES[card[0]] }
+end
